ab140e5a67279d408330033cd74a9760baaa6436
tasajos <tasajos@gmail.com>
2023-07-27 09:33:47 -0400

    update servicios

---
+++ src/app/Ifaz/agendaInter.ts
@@ -0,0 +1,13 @@
+export interface registrarcontactoInter {
+    // columna db 
+    id?: number,
+    nombre: string,
+    tipo:string,
+    email:string,
+    telefono:string,
+    ci:string,
+    extension:string,
+    fechanacimiento:Date,
+    estadocivil:string,
+    direccion:string,
+    }
